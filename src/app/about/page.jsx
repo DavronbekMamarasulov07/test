ca28beeb445b70989635a60a9e4760e0ebbe1d3d
DavronbekMamarasulov07 <nilufar12345davron@icloud.com>
2026-07-13 09:49:01 +0500

    first commit

---
+++ src/app/about/page.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import style from "./about.module.css"
+
+const About = () => {
+  return (
+      <div className={style.title}>
+          About
+      </div>
+  )
+}
+
+export default About
